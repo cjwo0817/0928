afefa56ee88096094b13aaed726cb326536126b7
cjwo0817 <54461251+cjwo0817@users.noreply.github.com>
2019-09-28 16:08:41 +0900

    Add files via upload

---
+++ Test01.py
@@ -0,0 +1,20 @@
+list1 = ['mom', 'fafa']
+list2 = ['me', 'brother', 'sister']
+list3 = ['grandma', 'grandfa']
+list4 = list1+list2+list3
+
+for i in range(len(list1)):
+    print(list1[i], end=' ')
+print('\n')
+
+for j in range(len(list2)):
+    print(list2[j], end=' ')
+print('\n')
+
+for k in range(len(list3)):
+    print(list3[k], end=' ')
+print('\n')
+
+for l in range(len(list4)):
+    print(list4[l], end=' ')
+
